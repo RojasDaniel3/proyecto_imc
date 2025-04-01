78ffd1d441936faadf6a721b9eae3d5c640e4f5c
Samuel Gutierrez <samucobaggg@gmail.com>
2025-03-31 19:19:43 -0500

    ya

---
+++ calcular_imc.py
@@ -0,0 +1,10 @@
+
+peso = print("Por favor ingrese su peso: ")
+peso = float(input())
+
+altura = print("Por favor ingrese su altura: ")
+altura = float(input())
+
+imc = peso / (altura**2)
+
+print(f"Su indice de masa muscular es de: {imc}")
